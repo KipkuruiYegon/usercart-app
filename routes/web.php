93342f8b6779467868449f7223bd0b484d45a8df
KipkuruiYegon <kevinkipkurui1@gmail.com>
2024-12-21 18:33:48 +0300

    User Dashboard redirection

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TemplateController;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard', [HomeController::class,'index']);
