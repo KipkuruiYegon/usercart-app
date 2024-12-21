18e8bfb5382d1eefb4da0eff8890f488b70df978
KipkuruiYegon <kevinkipkurui1@gmail.com>
2024-12-21 16:50:44 +0300

    template-theme intergration

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TemplateController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [TemplateController::class, 'index']);
